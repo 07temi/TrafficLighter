e467d21a30dd94892517912bd820a0b33ca6c1ba
Art <artem_chernenko@ro.ru>
2021-12-07 17:06:14 +0300

    code switching lights

--- TrafficLighter/ViewController.swift
+++ TrafficLighter/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
     }
 
-
+    @IBAction func startPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        
+    }
+    
 }
 
